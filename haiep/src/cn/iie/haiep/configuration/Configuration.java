a3b9b29cd48185ed135c7908198a784dfff40c6d
forhappy <haipingf@gmail.com>
2012-03-31 20:43:42 +0800

    add property configuration.

---
+++ haiep/src/cn/iie/haiep/configuration/Configuration.java
@@ -0,0 +1,11 @@
+package cn.iie.haiep.configuration;
+/**
+ * Configuration reader.
+ * @author forhappy
+ * Data: 2012-3-31 8:35 PM.
+ */
+import java.util.Properties;
+
+public class Configuration {
+
+}
