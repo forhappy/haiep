599d78a88315d08800c42ace684888b216a33eb7
forhappy <haipingf@gmail.com>
2012-04-06 10:15:18 +0800

    HKey implemented.

--- haiep/src/cn/iie/haiep/hbase/key/HKey.java
+++ haiep/src/cn/iie/haiep/hbase/key/HKey.java
@@ -1,5 +1,7 @@
 package cn.iie.haiep.hbase.key;
 
+import cn.iie.haiep.hbase.value.HValue.Value;
+
 /**
  * Key description in HBase.
  * @author forhappy
@@ -7,5 +9,28 @@ package cn.iie.haiep.hbase.key;
  */
 
 public class HKey {
+	public HKey(Value value) {
+		row = value.getBytes();
+	}
+	
+	public HKey(byte[] row) {
+		super();
+		this.row = row;
+	}
 
+	/**
+	 * @return the row
+	 */
+	public byte[] getRow() {
+		return row;
+	}
+
+	/**
+	 * @param row the row to set
+	 */
+	public void setRow(byte[] row) {
+		this.row = row;
+	}
+
+	private byte[] row = null;
 }
