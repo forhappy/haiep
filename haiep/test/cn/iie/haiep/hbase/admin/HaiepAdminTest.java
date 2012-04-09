c01a3bd39b1f4ee35e733a759a565aebf2e37f82
forhappy <haipingf@gmail.com>
2012-04-09 16:51:38 +0800

    add time consuming statistics for migration

--- haiep/test/cn/iie/haiep/hbase/admin/HaiepAdminTest.java
+++ haiep/test/cn/iie/haiep/hbase/admin/HaiepAdminTest.java
@@ -1,6 +1,17 @@
 package cn.iie.haiep.hbase.admin;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HaiepAdminTest {
+	public static void log(String msg) {
+		System.out.println(getTimeStamp() + " " + msg);
+	}
+
+	private static String getTimeStamp() {
+		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return f.format(new Date());
+	}
 	/**
 	 * @param args
 	 */
@@ -12,8 +23,13 @@ public class HaiepAdminTest {
 		
 		HaiepAdmin haiepAdmin = new HaiepAdmin(username, password, url, catalog);
 		
+		log("creating Scheam...");
 		haiepAdmin.initialize();
+		log("Scheam created...");
+		
+		log("Migration Launching...");
 		haiepAdmin.migrateData();
+		log("Migration Finished...");
 		
 	}
 }
